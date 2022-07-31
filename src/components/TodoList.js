07b2f985350b1142b1b4ae2644fe133451ff5941
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2022-07-31 20:58:54 +0600

    Finish functionality and add some icons

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import Todo from './Todo'
 import TodoForm from './TodoForm'
 
 function TodoList() {
   const [todos, setTodos] = useState([])
 
+  // Добавить туду
   const addTodo = (todo) => {
     if (!todo.text || /ˆ\s*/.test(todo.text)) {
       return
@@ -14,10 +16,45 @@ function TodoList() {
     setTodos(newTodos)
   }
 
+  // Изменить туду
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /ˆ\s*/.test(newValue.text)) {
+      return
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    )
+  }
+
+  // Удалить туду
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id)
+
+    setTodos(removeArr)
+  }
+
+  // Отправить туду в неактивный
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
   return (
     <div>
       <h1>What's the plan for today?</h1>
       <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   )
 }
